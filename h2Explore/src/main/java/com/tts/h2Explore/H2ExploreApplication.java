1cf6cf39b3cd7c24c2accdbd48da2e1df9ef6b0e
Tywon Sherman <ty1sherman@gmail.com>
2020-12-08 20:15:29 -0500

    h2Explore database has error

---
+++ h2Explore/src/main/java/com/tts/h2Explore/H2ExploreApplication.java
@@ -0,0 +1,13 @@
+package com.tts.h2Explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2ExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2ExploreApplication.class, args);
+	}
+
+}
